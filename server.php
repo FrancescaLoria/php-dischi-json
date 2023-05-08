bdc7fef8f407220aefc6a172d7f91d88602b84b1
FrancescaLoria <francesca.loria90@hotmail.it>
2023-05-08 16:20:17 +0200

    creazione API

--- server.php
+++ server.php
@@ -1,6 +1,13 @@
 <?php 
 $stringJsonValue = file_get_contents("dischi.json");
-var_dump($stringJsonValue);
+// var_dump($stringJsonValue);
 $dischi_array = json_decode($stringJsonValue, true);
-var_dump($dischi_array);
+// var_dump($dischi_array);
+
+// preparazione risposta
+$response = [
+    "results" => $dischi_array
+];
+header('Content-Type: application/json');
+echo json_encode($response);
 ?>
